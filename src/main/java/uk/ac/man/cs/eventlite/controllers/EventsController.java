fe880a781148baac06d5569c4f3947b0c68f4116
Russell Tang <wei.tang-13@student.manchester.ac.uk>
2022-03-10 18:07:34 +0000

    add event fix

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -1,13 +1,18 @@
 package uk.ac.man.cs.eventlite.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -85,6 +90,36 @@ public class EventsController {
 		e.setTime(event.getTime());
 		e.setVenue(event.getVenue());
 		eventService.save(e);
+		return "redirect:/events";
+	}
+	
+	@RequestMapping(value = "/newEvent", method = RequestMethod.GET)
+	public String newEvent(Model model) {
+		if (!model.containsAttribute("event")) {
+			model.addAttribute("event", new Event());
+		}
+
+		if (!model.containsAttribute("venueList")) {
+			model.addAttribute("venueList", venueService.findAll());
+		}
+
+		return "events/newEvent";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public String createEvent(@RequestBody @Valid @ModelAttribute Event event, 
+			BindingResult errors, Model model, RedirectAttributes redirectAttrs) {
+
+		if (errors.hasErrors()) {
+			model.addAttribute("event", event);
+			model.addAttribute("venueList", venueService.findAll());
+
+			return "events/new";
+		}
+
+		eventService.save(event);
+		redirectAttrs.addFlashAttribute("ok_message", "New event added.");
+
 		return "redirect:/events";
 	}
 	
