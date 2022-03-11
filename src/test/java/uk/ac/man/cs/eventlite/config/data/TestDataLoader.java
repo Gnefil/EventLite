8ec5a4511132ee7515beb1157eeddd090da12228
c41354lq <lifeng.qiulin@student.manchester.ac.uk>
2022-03-11 12:09:41 +0000

    Repopulate test data as example data and ready to write test
    Some details of the events are not specified in the script, in that
    case, the relevant fields are filled with arbitrary data. Other data
    fields are explicitly left empty, as example.
    
    Till this point, all tests should pass and the test data is the exemplar
    ones.

--- src/test/java/uk/ac/man/cs/eventlite/config/data/TestDataLoader.java
+++ src/test/java/uk/ac/man/cs/eventlite/config/data/TestDataLoader.java
@@ -34,13 +34,18 @@ public class TestDataLoader {
 			// Build and save test events and venues here.
 			// The test database is configured to reside in memory, so must be initialized
 			// every time.
-			Venue venue1 = new Venue("Kilburn", 100);
-			Venue venue2 = new Venue("MECD", 200);
-			log.info("Preloading: " + venueService.save(venue1));
-			log.info("Preloading: " + venueService.save(venue2));
-			log.info("Preloading: " + eventService.save(new Event("Team Study", venue1, LocalDate.now(), LocalTime.now(), "Software Engineering")));
-			log.info("Preloading: " + eventService.save(new Event("Lab", venue1, LocalDate.now(), LocalTime.now(), "Computer Science")));
-			log.info("Preloading: " + eventService.save(new Event("Lecture", venue2, LocalDate.now(), LocalTime.now(), "Computer Science")));
+			
+			Venue A = new Venue("Venue A", 100);
+			Venue B = new Venue("Venue B", 200);
+			log.info("Preloading: " + venueService.save(A));
+			log.info("Preloading: " + venueService.save(B));
+			log.info("Preloading: " + eventService.save(new Event("Event Alpha", B, LocalDate.of(2022, 7, 11), LocalTime.of(12, 30), "Event Alpha is the first of its kind…")));
+			log.info("Preloading: " + eventService.save(new Event("Event Beta", A, LocalDate.of(2022, 7, 11), LocalTime.of(10, 0), "Event Beta is not the first of its kind…")));
+			log.info("Preloading: " + eventService.save(new Event("Event Apple", A, LocalDate.of(2022, 7, 12), "Event Apple will be host to some of the world’s best iOS developers…"))); // No time
+			log.info("Preloading: " + eventService.save(new Event("Event Former", B, LocalDate.of(2022, 1, 11), LocalTime.of(11, 0), "Event Former happened long time ago.")));
+			log.info("Preloading: " + eventService.save(new Event("Event Previous", A, LocalDate.of(2022, 1, 11), LocalTime.of(18, 30)))); // No description
+			log.info("Preloading: " + eventService.save(new Event("Event Past", A, LocalDate.of(2022, 1, 10), LocalTime.of(17, 00), "Event Past happened long time ago.")));
+
 		};
 	}
 }
