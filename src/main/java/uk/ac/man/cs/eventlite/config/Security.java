94c85af8228d835c15628b88d96d8594c9834c4e
w77052dj <divin.jacob@student.manchester.ac.uk>
2022-04-29 16:42:25 +0100

    Create tests for venue update and fix failures

--- src/main/java/uk/ac/man/cs/eventlite/config/Security.java
+++ src/main/java/uk/ac/man/cs/eventlite/config/Security.java
@@ -30,7 +30,7 @@ public class Security extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		// By default, all requests are authenticated except our specific list.
 		http.authorizeRequests().requestMatchers(NO_AUTH).permitAll()
-			.antMatchers("/events/delete/**", "/events/update/**").hasRole(ADMIN_ROLE);
+			.antMatchers("/events/delete/**", "/events/update/**", "/events/newEvent", "/venues/delete/**", "/venues/update/**", "/venues/newVenue").hasRole(ADMIN_ROLE);
 		
 		// Use form login/logout for the Web.
 		http.formLogin().loginPage("/sign-in").permitAll();
