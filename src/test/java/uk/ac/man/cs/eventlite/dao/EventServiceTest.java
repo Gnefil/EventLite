2aec5c68b2a063442dccd9da101fea9497fb3d04
Yunyi Zhang <yunyi.zhang-2@student.manchester.ac.uk>
2022-04-29 02:14:33 +0100

    Add tests for findAllAndSort method on venue and event

--- src/test/java/uk/ac/man/cs/eventlite/dao/EventServiceTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/dao/EventServiceTest.java
@@ -1,6 +1,12 @@
 package uk.ac.man.cs.eventlite.dao;
 
-import org.junit.jupiter.api.Disabled;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,17 +16,44 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
 import uk.ac.man.cs.eventlite.EventLite;
+import uk.ac.man.cs.eventlite.entities.Event;
+import uk.ac.man.cs.eventlite.entities.Venue;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = EventLite.class)
 @DirtiesContext
 @ActiveProfiles("test")
-@Disabled
+
 public class EventServiceTest extends AbstractTransactionalJUnit4SpringContextTests {
 
 	@Autowired
 	private EventService eventService;
 
+	@Autowired
+	private VenueService venueService;
 	// This class is here as a starter for testing any custom methods within the
 	// EventService. Note: It is currently @Disabled!
+	
+	@Test
+	public void EventFindAllAndSortTest() throws Exception {
+		
+		Venue VA = new Venue();
+		venueService.save(VA);
+
+		Event C = new Event("C", VA, LocalDate.of(1000,01,02),
+				LocalTime.of(6,30));
+		eventService.save(C);
+		Event A = new Event("A", VA, LocalDate.of(1000,01,01),
+				LocalTime.of(0,0));
+		eventService.save(A);
+		Event B = new Event("B", VA, LocalDate.of(1000,01,02),
+				LocalTime.of(5,30));
+		eventService.save(B);
+		Iterable<Event> allEvents = eventService.findAllAndSort();
+		ArrayList<Event> eventsList = (ArrayList<Event>) allEvents;
+		
+		assertEquals(eventsList.get(0).getName(), "A");
+		assertEquals(eventsList.get(1).getName(), "B");
+		assertEquals(eventsList.get(2).getName(), "C");
+	}
 }
--- src/test/java/uk/ac/man/cs/eventlite/dao/VenueServiceTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/dao/VenueServiceTest.java
@@ -1,6 +1,6 @@
 package uk.ac.man.cs.eventlite.dao;
 
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,13 +9,17 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
+import java.util.ArrayList;
+
 import uk.ac.man.cs.eventlite.EventLite;
+import uk.ac.man.cs.eventlite.entities.Venue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = EventLite.class)
 @DirtiesContext
 @ActiveProfiles("test")
-@Disabled
+
 public class VenueServiceTest extends AbstractTransactionalJUnit4SpringContextTests {
 
 	@Autowired
@@ -23,4 +27,17 @@ public class VenueServiceTest extends AbstractTransactionalJUnit4SpringContextTe
 
 	// This class is here as a starter for testing any custom methods within the
 	// VenueService. Note: It is currently @Disabled!
+	
+	@Test
+	public void VenueFindAllAndSortTest() throws Exception {
+		Venue B = new Venue("B", "23 Manchester Road", "E14 3BD", 50);
+		venueService.save(B);
+		Venue A = new Venue("A", "23 Manchester Road", "E14 3BD", 50);
+		venueService.save(A);
+		Iterable<Venue> allVenues = venueService.findAllAndSort();
+		ArrayList<Venue> venuesList = (ArrayList<Venue>) allVenues;
+		assertEquals(venuesList.get(0).getName(), "A");
+		assertEquals(venuesList.get(1).getName(), "B");
+	}
+	
 }
