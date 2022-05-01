d91be4c65aa5098f35855d32342e9d96e09b4ee1
w77052dj <divin.jacob@student.manchester.ac.uk>
2022-05-01 15:20:51 +0100

    Test twitter error handling

--- src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/EventsController.java
@@ -193,10 +193,17 @@ public class EventsController {
 	@RequestMapping(value="/tweet/{id}", method= RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
 	public String updateStatusOnTwitter(@PathVariable("id") Long id, String tweet, RedirectAttributes redirectAttrs) {
 		try {
-			eventService.shareTweet(tweet);
-			redirectAttrs.addFlashAttribute("response",tweet);
+			tweet = tweet.trim();
+			if(tweet.length() == 0) {
+				redirectAttrs.addFlashAttribute("error","Tweet is Empty");
+			} else {
+				eventService.shareTweet(tweet);
+				redirectAttrs.addFlashAttribute("response",tweet);
+			}
+			
 		} catch (TwitterException e) {
 			e.printStackTrace();
+			redirectAttrs.addFlashAttribute("error","Unkown Error");
 		}
 
 		return "redirect:/events/details/{id}";
