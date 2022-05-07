80d4765d19265b5cc09f07c354a93615ce24b95c
u82130as <ahmed.soliman-2@student.manchester.ac.uk>
2022-05-07 23:20:45 +0100

    Completed Venue deletion coverage and fixed some typos

--- src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesController.java
+++ src/main/java/uk/ac/man/cs/eventlite/controllers/VenuesController.java
@@ -24,7 +24,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-
 import uk.ac.man.cs.eventlite.dao.EventService;
 import uk.ac.man.cs.eventlite.dao.VenueService;
 import uk.ac.man.cs.eventlite.entities.Event;
@@ -32,6 +31,7 @@ import uk.ac.man.cs.eventlite.entities.Venue;
 import uk.ac.man.cs.eventlite.exceptions.EventNotFoundException;
 import uk.ac.man.cs.eventlite.exceptions.VenueNotFoundException;
 import uk.ac.man.cs.eventlite.dao.EventRepository;
+
 @Controller
 @RequestMapping(value = "/venues", produces = { MediaType.TEXT_HTML_VALUE })
 public class VenuesController {
@@ -41,9 +41,9 @@ public class VenuesController {
 
 	@Autowired
 	private VenueService venueService;
-	
+
 	String MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiYXNvbGkiLCJhIjoiY2wxYWl3NzUyMXk3bTNpc2d4a3BrYmlpMiJ9.T1Lq2KsPQlAuWIAhg1Lh2g";
-	
+
 	@ExceptionHandler(EventNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public String venueNotFoundHandler(VenueNotFoundException ex, Model model) {
@@ -53,7 +53,7 @@ public class VenuesController {
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String getAllVenus(Model model) {
+	public String getAllVenues(Model model) {
 
 		model.addAttribute("venues", venueService.findAllAndSort());
 		return "venues/index";
@@ -63,9 +63,9 @@ public class VenuesController {
 	public String getVenueUpdate(@PathVariable("id") long id, Model model, RedirectAttributes redirectAttrs) {
 		Venue v = venueService.getVenueById(id);
 		if (v == null) {
-	 		redirectAttrs.addFlashAttribute("error_message", "venue not found");
-	 	}
-	 	
+			redirectAttrs.addFlashAttribute("error_message", "venue not found");
+		}
+
 		model.addAttribute("venue", v);
 		model.addAttribute("allVenues", venueService.findAll());
 		return "venues/update";
@@ -74,79 +74,78 @@ public class VenuesController {
 	@GetMapping("/details/{id}")
 	public String getVenueDetails(@PathVariable("id") long id, Model model) {
 		Venue v = venueService.getVenueById(id);
-		if(v == null) throw new EventNotFoundException(id);
-		
+		if (v == null)
+			throw new EventNotFoundException(id);
+
 		List<Event> upcoming = new ArrayList<Event>();
-				
-		for(Event event: eventService.findAllAndSort()) {
-			if (event.getVenue() == v && event.getDate().isAfter(LocalDate.now())) 
+
+		for (Event event : eventService.findAllAndSort()) {
+			if (event.getVenue() == v && event.getDate().isAfter(LocalDate.now()))
 				upcoming.add(event);
 		}
-		
+
 		model.addAttribute("venue", v);
 		model.addAttribute("upcomingEvents", upcoming);
-		
+
 		return "venues/details/index";
 	}
-	
-	 @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
-	 public String deleteById(@PathVariable("id") long id) {
-		 boolean isVenueEmpty = true;
-		 
-		 Iterable<Event> events = eventService.findAll();
-		 
-		 for(Event event : events) {
-			 Venue venue = event.getVenue();
-			 
-			 if(venue.getId() == id) {
-				 isVenueEmpty = false;
-			 }
-		 }
-		 
-		 if(isVenueEmpty) {
-			 venueService.deleteById(id);
-		 }
+
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+	public String deleteById(@PathVariable("id") long id) {
+		boolean isVenueEmpty = true;
+		Iterable<Event> events = eventService.findAll();
+
+		for (Event event : events) {
+			Venue eventVenue = event.getVenue();
+
+			if (eventVenue.getId() == id) {
+				isVenueEmpty = false;
+			}
+		}
+
+		if (isVenueEmpty) {
+			venueService.deleteById(id);
+		}
 		return "redirect:/venues";
-	 } 
-	 
-	 @RequestMapping(value="/update/{id}", method= RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	 public String updateVenue(@RequestBody @Valid @ModelAttribute Venue venue, BindingResult errors, Model model, @PathVariable("id") long id, RedirectAttributes redirectAttrs) {
-		 if (errors.hasErrors()) {
-			 model.addAttribute("venue", venue);
-			 return "venues/update";
-		 }
-		 Iterable<Event> events = eventService.findAll();
-		 
-		 Venue v = venueService.getVenueById(id);
-		 v.setName(venue.getName());
-		 v.setCapacity(venue.getCapacity());
-		 v.setRoadName(venue.getRoadName());
-		 v.setPostcode(venue.getPostcode());
-		 
-		 for(Event event : events) {
-			 Venue ven = event.getVenue();
-			 
-			 if(ven.getId() == id) {
-				 event.setVenue(v);
-				 String summ = event.getName() + " | " + v.getName() + " | " + event.getDate().toString();
-				 event.setSummary(summ);
-			 }
-		 }
-		 
-		 
-		 venueService.save(v);
-		 return "redirect:/venues";
 	}
-	
-	
-	@GetMapping(value="/search")
+
+	@RequestMapping(value = "/update/{id}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public String updateVenue(@RequestBody @Valid @ModelAttribute Venue venue, BindingResult errors, Model model,
+			@PathVariable("id") long id, RedirectAttributes redirectAttrs) {
+		if (errors.hasErrors()) {
+			model.addAttribute("venue", venue);
+			return "venues/update";
+		}
+		Iterable<Event> events = eventService.findAll();
+
+		Venue v = venueService.getVenueById(id);
+		v.setName(venue.getName());
+		v.setCapacity(venue.getCapacity());
+		v.setRoadName(venue.getRoadName());
+		v.setPostcode(venue.getPostcode());
+
+		for (Event event : events) {
+			Venue ven = event.getVenue();
+
+			if (ven.getId() == id) {
+				event.setVenue(v);
+				String summ = event.getName() + " | " + v.getName() + " | " + event.getDate().toString();
+				event.setSummary(summ);
+			}
+		}
+
+		venueService.save(v);
+		return "redirect:/venues";
+	}
+
+	@GetMapping(value = "/search")
 	public String searchVenuesByName(Model model, @Param("keyWords") String keyWords) {
 
 		model.addAttribute("venuesFound", venueService.search(keyWords));
 
 		return "venues/search";
 	}
-	
+
 	@RequestMapping(value = "/newVenue", method = RequestMethod.GET)
 	public String newVenue(Model model) {
 		if (!model.containsAttribute("venue")) {
@@ -155,23 +154,21 @@ public class VenuesController {
 
 		return "venues/newVenue";
 	}
-	
-	@RequestMapping(value="/newVenue", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-	public String createVenue(@RequestBody @Valid @ModelAttribute Venue venue, 
-			BindingResult errors, Model model, RedirectAttributes redirectAttrs) {
+
+	@RequestMapping(value = "/newVenue", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public String createVenue(@RequestBody @Valid @ModelAttribute Venue venue, BindingResult errors, Model model,
+			RedirectAttributes redirectAttrs) {
 
 		if (errors.hasErrors()) {
 			model.addAttribute("venue", venue);
-			
+
 			return "venues/newVenue";
 		}
-		
+
 		venue = venueService.geocode(venue, MAPBOX_ACCESS_TOKEN);
 		venueService.save(venue);
-		
-		venueService.save(venue);
-		redirectAttrs.addFlashAttribute("ok_message", "New venue added.");	
-		
+		redirectAttrs.addFlashAttribute("ok_message", "New venue added.");
+
 		return "redirect:/events";
 	}
 }
