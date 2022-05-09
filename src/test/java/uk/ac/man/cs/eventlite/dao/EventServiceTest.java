61f5d200dc51ad3dcdf050bd047908262bbb0986
Yunyi Zhang <yunyi.zhang-2@student.manchester.ac.uk>
2022-05-09 10:55:32 +0100

    Modify naming and ordering test more accurately in eventlistSorting test

--- src/test/java/uk/ac/man/cs/eventlite/dao/EventServiceTest.java
+++ src/test/java/uk/ac/man/cs/eventlite/dao/EventServiceTest.java
@@ -80,26 +80,26 @@ public class EventServiceTest extends AbstractTransactionalJUnit4SpringContextTe
 	// EventService. Note: It is currently @Disabled!
 	
 	@Test
-	public void EventFindAllAndSortTest() throws Exception {
+	public void testEventlistSorting() throws Exception {
 		
 		Venue VA = new Venue();
 		venueService.save(VA);
 
-		Event C = new Event("C", VA, LocalDate.of(1000,01,02),
+		Event C = new Event("pingpong1_", VA, LocalDate.of(1000,01,02),
 				LocalTime.of(6,30));
 		eventService.save(C);
-		Event A = new Event("A", VA, LocalDate.of(1000,01,01),
+		Event A = new Event("Hunt1_", VA, LocalDate.of(1000,01,01),
 				LocalTime.of(0,0));
 		eventService.save(A);
-		Event B = new Event("B", VA, LocalDate.of(1000,01,02),
-				LocalTime.of(5,30));
+		Event B = new Event("match1_", VA, LocalDate.of(1000,01,02),
+				LocalTime.of(16,30));
 		eventService.save(B);
 		Iterable<Event> allEvents = eventService.findAllAndSort();
 		ArrayList<Event> eventsList = (ArrayList<Event>) allEvents;
 		
-		assertEquals(eventsList.get(0).getName(), "A");
-		assertEquals(eventsList.get(1).getName(), "B");
-		assertEquals(eventsList.get(2).getName(), "C");
+		assertEquals(eventsList.get(0).getName(), "Hunt1_");
+		assertEquals(eventsList.get(1).getName(), "match1_");
+		assertEquals(eventsList.get(2).getName(), "pingpong1_");
 	}
 	
 	@Test
